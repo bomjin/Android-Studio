523fa5e58d3ea20c9c9506307a01fa1a8297673f
bumjin <bom96627@naver.com>
2019-05-10 00:47:13 +0900

    5/10일 commit

---
+++ app/src/main/java/com/example/bumjin/autocompletetextview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.bumjin.autocompletetextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items = {"뉴욕", "라스베가스", "마이애미", "Friends", "Fringe", "Lost"};
+
+        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
+        //arrayAdapter 뷰와 데이터를 연결해준다.
+        auto.setAdapter(adapter); //setAdapter 메소드를 이용한다.
+
+        MultiAutoCompleteTextView multi = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
+        MultiAutoCompleteTextView.CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(token);
+        multi.setAdapter(adapter);
+
+    }
+}
